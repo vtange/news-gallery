290bf042fe28dc27c9c1e554a6630bbb2278d681
Victor Tang <victortang1992@gmail.com>
2015-11-15 02:50:06 -0800

    comments

--- app.js
+++ app.js
@@ -4,34 +4,21 @@
 
 app.factory('memory', function($http){
 
-  var baseUrl = "http://www.freecodecamp.com/news/hot";
+  var baseUrl = "http://www.freecodecamp.com/news/hot";//for some reason this site can be HTTPGet'ed for json
 
   var storage = {};
  storage.datadata = [];
-                                                $http.get(baseUrl + "?callback=JSON_CALLBACK").success(function(data) {
+                                                $http.get(baseUrl + "?callback=JSON_CALLBACK").success(function(data) {//for some reason this site can be HTTPGet'ed for json
                                                     // you can do some processing here
                                                      storage.datadata = data;
                                                     console.log(storage.datadata);
-                                                }); 
-    //end info pulling
-
+                                                });
+                                                //end info pulling
   return storage;
 });//end of service
 
 app.controller('MainCtrl', ['$scope', 'memory', function($scope, memory){
     $scope.storage = memory; // load service
-
-    $scope.checkPic = function(user){
-        if (user.image == "") {
-            return false;
-        }
-        else {
-         return true;
-        };
-    }
-
-
-    
 }]);//end of controller
   //end of function
 })();
